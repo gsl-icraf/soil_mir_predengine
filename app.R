874a15b82169c298fff5265833df892573ec07cb
tvagen <tvagen@gmail.com>
2026-02-03 21:23:02 +0300

    Adjusted max upload size limit to 50MB

--- app.R
+++ app.R
@@ -22,12 +22,12 @@ source("modules/instructions_module.R")
 source("modules/property_info_module.R")
 
 # Set maximum upload size to 250 MB
-options(shiny.maxRequestSize = 250 * 1024^2)
+options(shiny.maxRequestSize = 50 * 1024^2)
 # Mirai daemons
 daemons(n = 2L, dispatcher = TRUE)
 # Source prediction processing function
 source("src/spectra_process_predict.R")
-options(shiny.maxRequestSize = 250 * 1024^2)
+options(shiny.maxRequestSize = 50 * 1024^2)
 # Define UI with URL-aware navigation
 ui <- function(request) {
   # Parse URL to determine initial page
